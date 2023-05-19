b114d7967137b609065f35f7747be3308f09a2b5
JohnMejia <john.mejias@udea.edu.co>
2023-05-18 22:52:01 -0500

    se agrega ms-compras

---
+++ backend/spring-boot-microservicio-compra/src/main/java/com/mejia/SpringBootMicroservicioCompraApplication.java
@@ -0,0 +1,13 @@
+package com.mejia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicioCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicioCompraApplication.class, args);
+	}
+
+}
